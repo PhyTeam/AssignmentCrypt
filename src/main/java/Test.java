96a2bd26f605d7ac4be0eeb9b8823ba1afe49dca
PhyTeam <buibaphuc95@gmail.com>
2017-03-02 23:36:10 +0700

    Update GUI

---
+++ src/main/java/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bbphuc
+ */
+
+import java.security.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+public class Test {
+    public void test(){
+        Cipher c;
+        
+    }
+    
+    
+}
